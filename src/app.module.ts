4edc344c50c020157302a8a14b6b068416f83f17
LeandroSousaDev <leandro_s.sousa@hotmail.com>
2025-02-23 13:45:34 -0300

    atualização pastas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TaskModule } from './task/task.module';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [TaskModule, UserModule, PrismaModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule { }
